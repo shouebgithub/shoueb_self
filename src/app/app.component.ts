12eec3bd46fb4cd07223c1841268caffe233f9e0
shoueb ahmed <shoueb081224@gmail.com>
2024-12-12 23:10:51 +0530

    second day

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
 
@@ -12,4 +12,11 @@ export class AppComponent {
 city = "Chicago"  
 users = [{id: 0, name: 'Sarah'}, {id: 1, name: 'Amy'}, {id: 2, name: 'Rachel'}, {id: 3, name: 'Jessica'}, {id: 4, name: 'Poornima'}]
 isEditable: boolean = true;
+// @Input() name = ''
+itemCount = 0;
+countShow(data: number) {
+  // this.items.push(item);
+ this.itemCount = data;
+  
+}
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,7 +8,27 @@ import { Component } from '@angular/core';
 })
 export class UserComponent {
   username = "shoueb ahmed";
-  isLoggedin = true
-  isServerRunning: boolean = false
+  message = '' 
+  isLoggedin = true;
+  private count = 0;
+  isServerRunning: boolean = false;
   operatingSystems = [{ id: 'win', name: 'Windows' }, { id: 'osx', name: 'MacOS' }, { id: 'linux', name: 'Linux' }];
+  @Input() name = ""
+ greet():void {
+   alert(`Welcome to Event Binding ${this.username}`);
+  }
+  onMouseOver() {
+    // this.message = "On the way to event binding using Event Handler";
+    // alert("mouse over working");
+    console.log("welcome to event handler");    
+  }
+  @Output() incrementCountEvent  = new EventEmitter<number>()
+
+  onClick() { 
+    this.count++
+    this.incrementCountEvent.emit(this.count);
+    // console.log(this.count);
+  }
+
+
 }
